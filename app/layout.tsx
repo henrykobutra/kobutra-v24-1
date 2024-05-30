d93c3ef32e5ace4601a70a93ac0482d116ce0fed
Henry Kobutra <henrykobutra@gmail.com>
2024-05-29 21:07:27 -0500

    Added SEO and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,41 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Henry Kobutra - Product Leader",
-  description: "Trilingual | Building AI Products",
+  title: 'Henry Kobutra - Product Leader',
+  description: 'Trilingual | Building AI Products',
+  authors: { name: 'Henry Kobutra', url: 'https://kobutra.com' },
+  creator: 'Henry Kobutra',
+  keywords: [
+    'Henry',
+    'Henry Kobutra',
+    'Kobutra',
+    'Varit Kobutra',
+    'product leader',
+    'ai',
+    'product management',
+    'trilingual',
+  ],
+  viewport: 'width=device-width, initial-scale=1',
+  robots: 'index, follow',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://kobutra.com',
+    siteName: 'Henry Kobutra',
+    title: 'Henry Kobutra - Product Leader',
+    description: 'Trilingual | Building AI Products',
+    images: 'https://kobutra.com/henry_sticky_notes.jpg',
+  },
+  twitter: {
+    creator: '@henrykobutra',
+    site: '@henrykobutra',
+    card: 'summary_large_image',
+    images: 'https://kobutra.com/henry_sticky_notes.jpg',
+  },
 };
 
 export default function RootLayout({
